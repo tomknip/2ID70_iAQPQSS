bbec1412e4cca1101394dc3a4bcbaf382b527ce8
tomknip <45173142+tomknip@users.noreply.github.com>
2019-03-07 23:02:53 +0100

    Update q2Queries.sql

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,8 +1,8 @@
-SELECT 0
-SELECT 0
-SELECT 0
+SELECT 0;
+SELECT 0;
+SELECT 0;
 SELECT count(CASE WHEN S.Gender = 'F'  THEN 1 END)/count(SR.StudentId)::float as percentageFemale FROM Degrees D, StudentRegistrationsToDegrees SR, Students S WHERE SR.DegreeId = D.DegreeId and S.StudentId = SR.StudentId and D.Dept = %1%;
 SELECT CR.CourseId, count(CASE WHEN CR.Grade >= %1% THEN 1 END)/count(CR.Grade)::float as percentagePassing FROM CourseRegistrations CR WHERE CR.Grade IS NOT NULL GROUP BY CR.CourseId ORDER BY CR.CourseId;
-SELECT 0
-SELECT 0
-SELECT 0
+SELECT 0;
+SELECT 0;
+SELECT 0;
