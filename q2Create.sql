0358c4b631d0601c57e6bd2d42dd32f481201a3e
Tom van Meer <tom.vmeer@gmail.com>
2019-03-08 16:45:42 +0100

    Update q2Create.sql

--- q2Create.sql
+++ q2Create.sql
@@ -3,4 +3,3 @@ SELECT  CR.studentid, CR.studentregistrationid, sum(ECTS), sum(CR.Grade * C.ECTS
 FROM courseRegistrations CR, CourseOffers CO, Courses C
 WHERE CR.CourseOfferId = CO.CourseOfferId And CO.CourseId = C.CourseId and CR.Grade >= 5
 GROUP BY CR.studentid, CR.studentregistrationid;
-CREATE INDEX idx_studRegIdandCourseOfferI on courseRegistrations(studentRegistrationId, courseofferId);
