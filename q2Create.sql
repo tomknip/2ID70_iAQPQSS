f2225d929fab4a9aba03b06fe35d7e5e429eb25d
tomknip <45173142+tomknip@users.noreply.github.com>
2019-03-08 19:20:51 +0100

    Update q2Create.sql

--- q2Create.sql
+++ q2Create.sql
@@ -1,5 +1,1 @@
-CREATE MATERIALIZED VIEW pointsPerS(studentId, studentregistrationid, sumECTS, GPA) as
-SELECT  CR.studentid, CR.studentregistrationid, sum(ECTS), sum(CR.Grade * C.ECTS)/sum(C.ECTS)
-FROM courseRegistrations CR, CourseOffers CO, Courses C
-WHERE CR.CourseOfferId = CO.CourseOfferId And CO.CourseId = C.CourseId and CR.Grade >= 5
-GROUP BY CR.studentid, CR.studentregistrationid;
+CREATE INDEX idx_gender ON students(gender);
