859d4f0c315223e2cb160215fc971e3be11db3d9
tomknip <45173142+tomknip@users.noreply.github.com>
2019-03-10 20:06:11 +0100

    Update q2Create.sql

--- q2Create.sql
+++ q2Create.sql
@@ -2,3 +2,4 @@ CREATE INDEX idx_gender ON students(gender);
 CREATE INDEX idx_studentIdAndDegreeIdAndGrade ON courseRegistrations(studentId,degreeId,grade);
 CREATE MATERIALIZED VIEW pointsPerS(studentId, studentregistrationid, sumECTS, GPA) as SELECT CR.studentid, CR.studentregistrationid, sum(CO.ECTS), sum(CR.Grade * CO.ECTS)/sum(CO.ECTS)::float FROM courseRegistrations CR, CourseOffers CO WHERE CR.CourseOfferId = CO.CourseOfferId and CR.Grade >= 5 GROUP BY CR.studentid, CR.studentregistrationid;
 CREATE MATERIALIZED VIEW studentNoFails(studentRegistrationId) as SELECT CR.StudentRegistrationID FROM courseRegistrations CR EXCEPT SELECT CR2.StudentRegistrationId FROM CourseRegistrations CR2 WHERE CR2.grade < 5;
+CREATE MATERIALIZED VIEW have_not_taken_yet(studentregistrationid, sumECTS) as (SELECT studentregistrationid, 0 FROM (SELECT studentregistrationid FROM studentRegistrationsToDegrees D EXCEPT SELECT studentregistrationid FROM pointsPerS) as new)
