af2a9cf0cd798c729dc85f5d0e2027c51dbd9dec
blazejmanczak <manczakb@gmail.com>
2019-03-10 18:48:58 +0100

    This index will make query 1 fly.

--- q2Create.sql
+++ q2Create.sql
@@ -1,3 +1,3 @@
 CREATE INDEX idx_gender ON students(gender);
-CREATE INDEX idx_studentIdAndDegreeId ON StudentRegistrationsToDegrees(studentId,degreeId);
+CREATE INDEX idx_studentIdAndDegreeId ON courseRegistrations(studentId,degreeId);
 CREATE MATERIALIZED VIEW pointsPerS(studentId, studentregistrationid, sumECTS, GPA) as SELECT CR.studentid, CR.studentregistrationid, sum(CO.ECTS), sum(CR.Grade * CO.ECTS)/sum(CO.ECTS) FROM courseRegistrations CR, CourseOffers CO WHERE CR.CourseOfferId = CO.CourseOfferId and CR.Grade >= 5 GROUP BY CR.studentid, CR.studentregistrationid;
