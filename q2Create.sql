c6f8f10b3c30d7ffb2aee83230c88acd54b182b1
blazejmanczak <manczakb@gmail.com>
2019-03-10 18:57:17 +0100

    Index to Speed up query 1 god dammit

--- q2Create.sql
+++ q2Create.sql
@@ -1,3 +1,3 @@
 CREATE INDEX idx_gender ON students(gender);
-CREATE INDEX idx_studentIdAndDegreeId ON courseRegistrations(studentId,degreeId);
+CREATE INDEX idx_studentIdAndDegreeIdAndGrade ON courseRegistrations(studentId,degreeId,grade);
 CREATE MATERIALIZED VIEW pointsPerS(studentId, studentregistrationid, sumECTS, GPA) as SELECT CR.studentid, CR.studentregistrationid, sum(CO.ECTS), sum(CR.Grade * CO.ECTS)/sum(CO.ECTS) FROM courseRegistrations CR, CourseOffers CO WHERE CR.CourseOfferId = CO.CourseOfferId and CR.Grade >= 5 GROUP BY CR.studentid, CR.studentregistrationid;
